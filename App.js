0b32126889925e70550cb1a30a5638db57310bad
lucifer123A <lokeshdulani91@gmail.com>
2021-01-11 12:18:23 +0530

    Introduced Navigation

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import {View,Text,StyleSheet} from 'react-native'
+import {createStackNavigator} from "@react-navigation/stack";
+import {NavigationContainer} from '@react-navigation/native'
+import SearchScreen from './src/screens/SearchScreen'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const Stack=createStackNavigator()
+const App=()=>{
+  return(
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Search">
+        <Stack.Screen 
+        name="Screen" 
+        component={SearchScreen}
+        options={{title:"Business Search"}}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
