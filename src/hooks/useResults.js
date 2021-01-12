98ab4455cc3829060a349147510e38ee4c5db87f
lucifer123A <lokeshdulani91@gmail.com>
2021-01-12 13:30:58 +0530

    Extracted Hooks

---
+++ src/hooks/useResults.js
@@ -0,0 +1,32 @@
+import {useState,useEffect} from 'react'
+import yelp from  '../api/yelp'
+
+export const useResults=()=>{
+    const [results,setResults]=useState([])
+    const [errorMessage,setErrorMessage]=useState('')
+
+    const searchAPI=async (searchTerm)=>{
+        console.log("Hi There!")
+        try{
+            const response = await yelp.get('/search',
+            {params:{
+                limit:50,
+                term:searchTerm,
+                location:'roma'
+            }})
+            setResults(response.data.businesses)
+        }
+        catch(e){
+            setErrorMessage("Something went wrong")
+            console.log(e)
+        }
+    }
+    //Bad code infinite loop
+    //searchAPI("pasta") , hence use useEffect hook
+
+    useEffect(()=>{
+        searchAPI('pizza')
+    },[])
+
+    return [searchAPI,results,errorMessage]
+}
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,31 +1,11 @@
-import React,{useState} from 'react'
+import React,{useState,useEffect} from 'react'
 import {View,Text,StyleSheet} from 'react-native'
 import SearchBar from '../components/SearchBar'
-import yelp from '../api/yelp'
+import { useResults } from "../hooks/useResults";
 
 const SearchScreen=()=>{
     const [term,setTerm] = useState('')
-    const [results,setResults]=useState([])
-    const [errorMessage,setErrorMessage]=useState('')
-
-    const searchAPI=async (searchTerm)=>{
-        console.log("Hi There!")
-        try{
-            const response = await yelp.get('/search',
-            {params:{
-                limit:50,
-                term:searchTerm,
-                location:'roma'
-            }})
-            setResults(response.data.businesses)
-        }
-        catch(e){
-            setErrorMessage("Something went wrong")
-            console.log(e)
-        }
-    }
-    //Bad code infinite loop
-    searchAPI("pasta")
+    const [searchAPI,results,errorMessage] = useResults()
     return(
         <View>
             <SearchBar term={term} onTermChange={setTerm} onTermSubmit={()=>searchAPI(term)}/>
